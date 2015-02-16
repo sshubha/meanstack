228f64e30141015c0da424cbfa02aacc8f71c825
root <suvarnas@cs.fsu.edu>
2015-02-16 00:29:23 -0500

    adding content to package.json and server.js files

---
+++ server.js
@@ -0,0 +1,21 @@
+// set up code
+var express = require('express');
+var app = express(); //creates the app with express
+var mongoose = require('mongoose'); //this code is for mongodb
+var morgan = require('morgan'); // used to log requests to the console of express
+var bodyParser = require('body-parser'); //used to pull info from html post
+var methosOverride =  require('method-override'); // simulate delete and put
+
+// configuration
+
+mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uw03mypu'); //connect to mongoDB on modulus.info
+
+app.use(express.static(_dirname + '/public')); //set the static files location tp /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); //parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); //parse application/json
+app.use(bodyParser.json({type:'application/vnd.api+json'})); //parse application/vnd.api+json as json
+app.use(methosOverride()); 
+
+app.listen(8080);
+console.log("The application started listening on port 8080");
